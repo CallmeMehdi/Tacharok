3e0da8ba761fc3d775427465df73ccf781afdf70
CallmeMehdi <47258917+CallmeMehdi@users.noreply.github.com>
2020-01-26 16:53:31 +0100

    Add files via upload

---
+++ Tacharok/assets/js/forumFade.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".fade-first").fadeIn(1200);
+        // var documentEl = $(document),
+        // fadeElem = $('.fade-scroll');
+        // console.log(fadeElem)
+        
+        // documentEl.on('scroll', function() {
+        //     var currScrollPos = documentEl.scrollTop();
+            
+        //     fadeElem.each(function() {
+        //         var $this = $(this),
+        //             elemOffsetTop = $this.offset().top;
+        //         if (currScrollPos > elemOffsetTop) $this.css('opacity', 1 - (currScrollPos-elemOffsetTop)/400);
+        //     }); 
+        // });
+        
+})
